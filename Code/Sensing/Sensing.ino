9dbe685c7f40190a4136cf44f255a7f7531443fd
nxsmpi001 <163320031+nxsmpi001@users.noreply.github.com>
2024-05-12 12:47:56 +0200

    Updated code

--- Code/Sensing/Sensing.ino
+++ Code/Sensing/Sensing.ino
@@ -12,23 +12,38 @@
 //********************************************************************
 // INCLUDE FILES
 //====================================================================
-
+#include "ADS1X15.h"
+#include <string>
 //====================================================================
 // GLOBAL CONSTANTS
 //====================================================================
-#define SIZE 10
 #define FALSE 0
 #define TRUE 1
-#define PERIOD 200
+#define SIZE 10
+#define PERIOD 100
+#define BUTTON_PIN 2
+#define LED_PIN 3
+#define DEBOUNCE_DELAY 250
 //====================================================================
 // GLOBAL VARIABLES
 //====================================================================
-float voltages[SIZE+1]; // Arrray of previous voltage values
-int i;                  // Index of current value 
-int full;               // Indicates whether array is full or not
+float weights[SIZE+1];     // Arrray of previous voltage values
+int i;                    // Index of current value 
+int full;                 // Indicates whether array is full or not
+float offset;             // Weight offset
+volatile long last_press; // Time since last button press
+ADS1115 ADS(0x48);        // Constructor for ADS1115 , default I2C address 0x48 is used
 //====================================================================
 // FUNCTION DECLARATIONS
 //====================================================================
+void buttonPressedISR() 
+{
+  if(millis() - last_press > DEBOUNCE_DELAY){
+    digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+    last_press = millis();
+  }
+}
+
 float movingAverage(float* values, int index)
 {
   float sum = values[SIZE]; 
@@ -43,45 +58,60 @@ float movingAverage(float* values, int index)
   return sum/SIZE;
 }
 
-float voltsToGrams(float Vout) {  // will update later
-  float Vin = 1/994*(2*Vout - 3.3);
-  
-  return Vin;  //Convert input voltage to weight
+float voltsToGrams(float V) { 
+  float weight = (V - 1.5935)/0.0015; //Convert input voltage to weight
+  return weight;  
 }
 //====================================================================
-// MAIN LOOP and SETUP
+// SETUP and LOOP
 //====================================================================
 void setup() 
 {
   // Initialise variables
   i = 0;
   full = FALSE;
-  voltages[SIZE] = 0;
+  weights[SIZE] = 0;
+  offset = 0;
+  last_press = 0;
 
   // Setup Serial connection
   Serial.begin(9600);
 
-  // Use internal voltage reference for ADC
-  analogReference(AR_INTERNAL);
+  // Setup the ADS1115
+  Wire.begin();
+  ADS.begin();
+  ADS.setGain(1); // Increase gain on pga for more precise measurements
+  ADS.setDataRate(1); // Set data rate to 16sps
 
-  // Change the ADC resolution to 12 bits
-  analogReadResolution(12);
+  // Setup input and output pins
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+  // Setup interrupt on push buttom
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), buttonPressedISR, FALLING);
+  // Set LED to initially be OFF
+  digitalWrite(LED_PIN, LOW);
 
 }
 
 void loop() 
 {
+  ADS.setGain(0);
+
   // Measure execution time
   long int t = millis();
 
-  // Read the voltage on A0
-  uint16_t adcVal = analogRead(A1);   // ADC value
-  float V = adcVal * 3.3 / 4096;      // Convert to a voltage
-  
+  // Read the voltage on channel A0
+  int16_t adcVal = ADS.readADC(0);  // Get adc value
+  float f = ADS.toVoltage(1);       // voltage factor
+  float V = adcVal * f;             // convert to a voltage
+  float weight = voltsToGrams(V);   // convert voltage to weight
+  // Apply offset to weight
+  weight = (weight-offset > 0) ? weight-offset : 0;
+
   if(!full) {
-    voltages[i] = V;
-    voltages[SIZE] += V;
-    V = voltages[SIZE]/(i+1);
+    weights[i] = weight;
+    weights[SIZE] += weight;
+    weight = weights[SIZE]/(i+1);
     i += 1;
     if(i>=SIZE) {
       full = TRUE;
@@ -90,18 +120,25 @@ void loop()
     
   }
   else {
-    voltages[i] = V;
-    V = movingAverage(voltages, i);
+    weights[i] = weight;
+    weight = movingAverage(weights, i);
     i = (i+1 < SIZE) ? i+1 : 0;
-    
   }
-  V = (1/1.161)*(V-0.09);
-  String str = "ADC Val = ";
-  Serial.print(str);
-  Serial.print(adcVal);
-  str = "\tV =  ";
-  Serial.print(str);
-  Serial.println(V);
+
+  if(digitalRead(LED_PIN)) {
+    if(offset<=0) {
+      offset = weight;
+    }
+  }
+  else {
+    offset = 0;
+  }
+
+  Serial.print("V = "); Serial.print(V);
+  char arr [15] = "";
+  sprintf(arr,"\tWeight = %.1f", weight);
+  String str(arr);
+  Serial.println(str);
 
   t = t-millis();
   delay(PERIOD-t);
